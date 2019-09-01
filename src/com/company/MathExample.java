9294c9d12d012958d5da3f8e37ede0071df50782
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-09-01 15:53:03 +0200

    Zadanie 5.2 i przykłady ze statycznymi importami

---
+++ src/com/company/MathExample.java
@@ -0,0 +1,13 @@
+package com.company;
+import com.company.traveloffice.Date;
+
+import static java.lang.Math.*;
+import static com.company.traveloffice.Date.*;
+
+public class MathExample {
+    public static void main(String[] args) {
+        double x = sqrt(8) * PI;
+        System.out.println(counter);
+        Date d = new Date();
+    }
+}
---
+++ src/com/company/ParametersExample.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import com.company.traveloffice.Date;
+
+public class ParametersExample {
+
+    static void f1(int a) {
+        a = a * 4;
+    }
+
+    static void f2(String s) {
+        s = s + "hahahaha";
+    }
+
+    static void f3(Date d) {
+        d.year= 3456;
+    }
+
+    public static void main(String[] args) {
+        int x = 8;
+        f1(x);
+        System.out.println(x);
+        String w = "pawel";
+        f2(w);
+        System.out.println(w);
+        Date d1 = new Date(2019, 3, 5);
+        f3(d1);
+        System.out.println(d1.getInfo());
+    }
+}
