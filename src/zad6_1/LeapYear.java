1044d958030f43ef6e0905c6fc1a8766f8379b45
florianpp <piotrekflo@poczta.fm>
2019-09-14 10:42:57 +0200

    Początkowe rozw zad 6.1

---
+++ src/zad6_1/LeapYear.java
@@ -0,0 +1,30 @@
+package zad6_1;
+import java.util.Scanner;
+
+public class LeapYear {
+    public static  boolean isLeapYear{
+        if (year %400==0) {
+            return true;
+        } else{
+            if (year %100==0){
+                return false;
+            } else {
+                if (year % 4 ==0){
+                    return true;
+                } else{
+                    return false;
+                }
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        while (scaner.hasNext()){
+            int year =
+        }
+
+    }
+}
