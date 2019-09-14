70412c99d8e23afb3e9a51eb6807174a89aac3ec
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-09-14 16:00:35 +0200

    Parametry programu

--- src/com/company/SumExample.java
+++ src/com/company/SumExample.java
@@ -1,13 +1,20 @@
 package com.company;
 
 public class SumExample {
-    public static int sum(int [] a) {
-        return 0;
+    public static int sum(int ... a) {
+        int suma = 0;
+        for (int element : a) {
+            suma += element;
+        }
+        return suma;
     }
 
     public static void main(String[] args) {
-//        int s1 = sum(1,2,3);
-//        int s2 = sum(1,2,4,6);
-//        int s3 = sum(9,7,4,2,5,7);
+        int s1 = sum(1,2,3);
+        int s2 = sum(new int[]{1,2,4,6});
+        int s3 = sum(new int[]{9,7,4,2,5,7});
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
